188fc83291a8b2d6ba1e2bcd1e11ff2d1c308809
Asilva613 <64608818+Asilva613@users.noreply.github.com>
2022-05-17 22:41:16 -0400

    Update draw_initials.s

--- draw_initials.s
+++ draw_initials.s
@@ -70,12 +70,6 @@ _start:
         mov r1,#0
         bl setPixel
         
-       
-        #set pixel 7,1 to the blue color stored in r6
-        mov r2,r6
-        mov r0,#7
-        mov r1,#1
-        bl setPixel
         
         
         #set pixel 7,3 to the blue color stored in r6
@@ -134,18 +128,13 @@ _start:
         bl setPixel
         
         
-        #set pixel 0,2 to the blue color stored in r6
+        #set pixel 4,2 to the blue color stored in r6
         mov r2,r6
-        mov r0,#0
+        mov r0,#4
         mov r1,#2
         bl setPixel
         
         
-        #set pixel 3,0 to the blue color stored in r6
-        mov r2,r6
-        mov r0,#3
-        mov r1,#0
-        bl setPixel
         
         #second initial
         #set pixel 0,4 to the blue color stored in r6
