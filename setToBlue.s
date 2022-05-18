8727fecd072a327b798bd4d826eaff4f56a9f95a
Asilva613 <64608818+Asilva613@users.noreply.github.com>
2022-05-17 23:30:14 -0400

    Update setToBlue.s

--- setToBlue.s
+++ setToBlue.s
@@ -1,53 +1,43 @@
-#Mbiet Uko
-#Conor McCloskey
-#Alejandro Silva
-
-.global _start
 .text
+.global _start
 _start:
-        #open frame buffer 1
-        mov r0,#1
-        bl openfb
-        cmp r0,#0
-        blt _start_exit
-        
-        
-        #get the color blue 
-        mov r3,#0
-        mov r4,#0
-        mov r5,#31
-        bl getColor
-        mov r6,r0
-        
-     
-        mov r0,#1
-        mov r1,#1
-startLoop:
-        cmp r0,#8
-        add r0,r0,#1
-        
-        bgt startLoop2
-        b startLoop
-        
-startLoop2:
-        cmp r1,#8
-        add r1,r1,#1
-        
-        #set pixel to the blue color stored in r6
-        mov r0,r0
-        mov r1,r1
-        mov r6,r6
-        bl setPixel
-        
-        bgt _start_exit
-        b startLoop2
-        
-        
-        #cleanup the data structures created by openfb and close the framebuffer device
-        bl closefb
-         
+
+    #open frame buffer 1
+    mov r0, #1
+    bl openfb
+    cmp r0, #0
+    mov r5, #8
+    mov r6, #7
+    blt _start_exit
+    
+    
+    #get the color blue and store it in r6
+    mov r0,#0
+    mov r1,#0
+    mov r2,#31
+    bl getColor
+    mov r10,r0
+
+l1:
+    cmp r5, #0
+    beq _start_exit
+    sub r5, r5, #1
+    mov r6, #7
+    b l2
+    b l1
+
+l2:
+    cmp r6, #-1
+    beq l1
+    mov r0, r5
+    mov r1, r6
+    mov r2, r10
+    bl setPixel
+    sub r6, r6, #1
+    b l2
+
+    bl closefb
 
 _start_exit:
-        #clean exit
-        mov r7,#1
-        svc #0
+    mov r7, #1
+    svc #0
