7b9d10a55249be069c95033c698f37c214b02667
mbiet11 <79806771+mbiet11@users.noreply.github.com>
2022-05-17 23:26:33 -0400

    Update firstclear.s

--- firstclear.s
+++ firstclear.s
@@ -1,53 +1,32 @@
-.data
-        FILENAME: .asciz "/dev/fb1"
-        FD: .word 0
-        FB: .word 0
-
-.global openfb
-.global closefb
-.global getColor
-.global setPixel
-
 .text
 .global _start
 _start:
 
-#open frame buffer
-mov r0, #1
-bl openfb
-cmp r0, #0
-blt _start_exit
-
-#set pi blank
-mov r8, r0
-mov r0,#0
-mov r2, #0
-bl getColor
-mov r6, r0
-
-#initialization
-mov r0, #0
-mov r1, #0
-mov r2, r6
-mov r3, #0
-mov r4, #0
-b l2
+    #open frame buffer 1
+    mov r0, #1
+    bl openfb
+    cmp r0, #0
+    mov r5, #8
+    mov r6, #7
+    mov r10, #0
+    blt _start_exit
 
 l1:
-    cmp r0, #8
+    cmp r5, #0
     beq _start_exit
-    add r4, #1
-    mov r1, #0
-    add r3, #0
+    sub r5, r5, #1
+    mov r6, #7
     b l2
+    b l1
 
 l2:
-    cmp r1, #0
+    cmp r6, #-1
     beq l1
+    mov r0, r5
+    mov r1, r6
+    mov r2, r10
     bl setPixel
-    mov r0, r12
-    add r3, #1
-    mov r1, r3
+    sub r6, r6, #1
     b l2
 
     bl closefb
