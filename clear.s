1ec25438494ebad20bea38b02042de19c888eb6d
Asilva613 <64608818+Asilva613@users.noreply.github.com>
2022-05-17 20:42:04 -0400

    Update clear.s

--- clear.s
+++ clear.s
@@ -5,3 +5,19 @@
 .global _start
 .text
 _start:
+        #open frame buffer 1
+        mov r0,#1
+        bl openfb
+        cmp r0,#0
+        blt _start_exit
+        
+        
+        
+        
+        #cleanup the data structures created by openfb and close the framebuffer device
+        bl closefb
+
+_start_exit:
+        #clean exit
+        mov r7,#1
+        svc #0
